eb3c6db644d0e58d7985b0cde2e6a6feddc7fab7
Joseph397 <gramkracker88@gmail.com>
2020-08-19 14:23:55 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+// Loops
+// repeatedly run a block of code while condition is true
+// for loop
+
+// let i;
+// for(i = 0; i < 10; i++){                   // (starting point; ending point; iteration)
+//  console.log('and the number is : ' + i);  // code to run
+// }
+
+for(let number = 11; number >= 0; number--) {    // number starts at 11; ends at greater than or equal to 0; iteratets by -1)
+    console.log('and the number is : ' + number);// repeatedly runs code until conditions are not met
+}
